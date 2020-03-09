15cac47b4f1986ffff1d4b0953b4b5cd3f4fa0af
Shilin Victor <chrono.monochrome@gmail.com>
2020-03-09 22:03:14 +0300

    HACK: smdk4412-qcom: fix build
    Change-Id: I38b442ed1f437e459d3123990653bc3a8618bc88

--- go_smdk4412-qcom.mk
+++ go_smdk4412-qcom.mk
@@ -22,7 +22,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
      persist.traced.enable=1
 
 # Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+#PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
