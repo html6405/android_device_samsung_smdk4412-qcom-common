8adcf03273bbda113422e0b9b537d1f6b5efc890
Shilin Victor <chrono.monochrome@gmail.com>
2020-03-09 20:56:55 +0300

    smd4412-qcom: import build.prop optimizations from smdk4412-common
    Change-Id: I03af59459610be8af2d39252093baab7c18c9029

--- common.mk
+++ common.mk
@@ -46,3 +46,14 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril.config=fakeiccid
+
+# Art
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-threads=1 \
+    dalvik.vm.image-dex2oat-threads=1
+
+# Android GO stuff
+$(call inherit-product, $(LOCAL_PATH)/go_smdk4412-qcom.mk)
+
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
