eb9c3818a57a503a8700287582948b0ffbb2f3ed
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-05-06 13:02:03 +0200

    Move tombstones preparations to qcks.sh.

--- qcks/qcks.sh
+++ qcks/qcks.sh
@@ -3,4 +3,12 @@
 mount /dev/block/mmcblk0p10 /firmware
 mount /dev/block/mmcblk0p11 /tombstones
 
+mkdir /data/cp_log
+chmod 0775 /data/cp_log
+chown system radio /data/cp_log
+chown system system /tombstones
+chmod 0775 /tombstones
+restorecon_recursive /tombstones
+restorecon /tombstones/qcks/temp.dump
+
 /system/vendor/bin/qcks s
